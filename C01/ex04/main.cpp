1727376dc16f3f808c6aaf4fb5e77b4b2a11d0fc
tcybak <tcybak@student.42.fr>
2025-10-07 22:07:09 +0200

    cpp01 start ex04

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,24 +6,43 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 14:52:27 by tcybak            #+#    #+#             */
-/*   Updated: 2025/10/07 16:42:11 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/10/07 22:06:23 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <string>
+
+std::string replace(std::string content, std::string replace)
+{
+    
+}
 
 int main(int ac, char **av)
 {
-
     if (ac != 4)
     {
         std::cout   << "Need 1 file and 2 arguments"
                     << std::endl;
         return (0);
     }
-    std::ifstream file(av[1]);
-    if (!file) { std::ofstream file(av[1]); }
+
     
+    std::string     toReplace = av[2];
+    std::string     replaceWith = av[3];
+    std::ifstream   file(av[1]);
     
+    if (!file || !file.is_open()) 
+    {
+        std::cerr << "Error file that does not exist or permission does not exist" << std::endl;
+        return (1);
+    }
+
+    std::string     fileContent;
+
+    std::getline(file, fileContent,'\0');
+    
+    std::cout << fileContent << std::endl;
+    file.replace(fileContent, replaceWith);
 }
