358e0eed523345ef984b656c9c003473f5b2951d
tcybak <tcybak@student.42.fr>
2025-10-10 15:26:30 +0200

    cpp02 ex00 start

---
+++ C02/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 15:12:40 by tcybak            #+#    #+#             */
+/*   Updated: 2025/10/10 15:17:30 by tcybak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include " Fixed.hpp"
+
+int main( void ) 
+{
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return (0);
+}
