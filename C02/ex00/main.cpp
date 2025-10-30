9da4eea6387e93d20ad4ee50985e24b773e91509
tcybak <tcybak@student.42.fr>
2025-10-30 18:48:21 +0100

    up change .aout

--- C02/ex00/main.cpp
+++ C02/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: timothy <timothy@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 15:12:40 by tcybak            #+#    #+#             */
-/*   Updated: 2025/10/24 10:07:37 by timothy          ###   ########.fr       */
+/*   Updated: 2025/10/30 18:24:21 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int main( void )
     Fixed b(a);
     Fixed c;
     c = b;
+    a.setRawBits(5);
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
